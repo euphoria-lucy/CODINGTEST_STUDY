27b2a5fd2e6e8d1b6ca7fbf60322402ee00b48ac
misung <voielactee7@naver.com>
2023-05-24 09:07:50 +0900

    전처리기하기

---
+++ CODINGTEST/CODINGTEST/예제2.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#define CNT 5
+
+int main() {
+	int a;
+	printf("-------------- \n");
+	for (a = 0; a <= CNT; a++) {
+		printf("a ¡æ %2d\n", a);
+	}
+	printf("-------------- \n");
+}
