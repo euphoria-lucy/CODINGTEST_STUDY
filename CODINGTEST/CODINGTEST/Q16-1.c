400dee8bc058c3d050253fb78df59ae60f22e13e
misung <voielactee7@naver.com>
2023-06-07 10:02:02 +0900

    [ALGORITHM] for문 이용해 숫자 출력_1

---
+++ CODINGTEST/CODINGTEST/Q16-1.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+main() {
+	int A[100][100];
+	int i, j, k = 0, n;
+
+	scanf_s("%d", &n);
+	for (i = 1; i <= n; i++) {
+		for (j = 1; j <= n; j++) {
+			k++;
+			A[i][j] = k;
+		}
+	}
+
+	for (i = 1; i <= n; i++) {
+		for (j = 1; j <= n; j++) {
+			printf("%5d", A[i][j]);
+		}
+		printf("\n");
+	}
+}
