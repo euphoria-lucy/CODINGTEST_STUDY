a7efd1633822c1630b6fdf3ae46051529efe6bff
MISUNG <113699085+euphoria-lucy@users.noreply.github.com>
2023-06-28 09:34:43 +0900

    [C] 정수값 읽어 포인터로 출력

---
+++ CODINGTEST/CODINGTEST/연산자.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+void main() {
+	int a = 3;
+	int *p_one = &a;
+	printf("%d", *p_one);
+	
+	return 0; 
+}
---
+++ CODINGTEST/CODINGTEST/연산자_3.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+main(){
+   int i;
+   int *ip = &i;
+   printf("정수입력: ");
+   scanf("%d ", &i);
+   printf("결과 = %d ",*ip+1);
+   printf("주소값= %d  %d",ip,ip+1);
+   
+   return 0; 
+}
